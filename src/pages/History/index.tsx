41b4adf0eeba78b58fb6fb21b2b15e48fa12825e
JoaoVitorNY <joaovitornakahodo@gmail.com>
2026-08-06 12:41:40 -0300

    fix: fix bug when clearing history outside History page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -68,6 +68,11 @@ export function History() {
 		dispatch({ type: TaskActionTypes.RESET_STATE })
 	}, [confirmClearHistory, dispatch])
 
+	useEffect(() => {
+		return () => {
+			showMessage.dismiss()
+		}
+	}, [])
 
 	return (
         <MainTemplate>
